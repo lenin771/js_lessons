4e7b9cc5a3d0ab3485ba420054238010ceb75201
unknown <yurist.nedvigimost@gmail.com>
2025-02-18 11:51:19 +0300

    add index.html, ex1.js

---
+++ js/ex1.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// alert("Hello");
+
+// let result = confirm('Yes');
+// console.log(result);
+
+// let age = prompt("Введите возраст", 18);
+// console.log(age);
+
+
+// let x = 1;
+
+// if (x < 0) console.log('x отриц');
+// else if (x > 0) console.log('x полож');
+// else console.log('x = 0')
+
+// let age = (x >= 18) ? true : false;
+// console.log(age);
+
+// let z = 1;
+// if(z >= 2 && z <= 7) console.log("z попадает в [2; 7]");
+// else console.log('z не попадаетв [2; 7]');
+
+// let item = -3;
+// switch(item) {
+//     case 1: console.log('item = 1'); break;
+//     case 2: console.log('item = 2'); break;
+//     case 3: console.log('item = 3'); break;
+//     case 4: console.log('item = 4'); break;
+//     default: console.log('item другое значение');
+// }
+
+
+// let s = 0;
+
+// while (s <= 100) {
+//     console.log(s, i);
+//     s += i++;
+// }
+// console.log(s, i);
+
+// for(let i = 1; s < 100; ++i) {
+//     s += i;
+//     console.log(s, i);
+// }
+// console.log(s);
+
+// const PSW = "pass";
+// let psw = null;
+// do {
+//     psw = prompt("Введите пароль","");
+// }
+// while(psw != PSW);
+// console.log("Вы вошли в систему!");
+
